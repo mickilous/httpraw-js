5b6fee3704f015fa0a14f659f6ef616f05ee2e0b
mickilous <mickilous@gmail.com>
2016-06-01 15:41:53 +0200

    IMPROVE - Port as parameter

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var express = require('express');
 var bodyParser = require('body-parser')
 
 var app = express();
+var port = process.argv[2]
+if (isNaN(port))
+    port = 9999
 
-app.use(bodyParser.raw({ type: '*/*' }))
+app.use(bodyParser.raw({type: '*/*'}))
 
 app.use(function (req, res, next) {
 
@@ -17,7 +20,8 @@ app.use(function (req, res, next) {
     logFields(req.query)
 
     console.log('--- Content ---')
-    console.log(req.body.toString())
+    if (req.body.length > 0)
+        console.log(req.body.toString())
 
     res.send('LOGGED')
     next()
@@ -32,6 +36,6 @@ function logFields(obj) {
 }
 
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+    console.log('Httpraw-js app listening on port', port, '!');
 });
